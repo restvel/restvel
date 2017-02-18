3486081229a9ceae912c61abe6f29af072ecac80
Farhan Wazir <farhan.wazir@gmail.com>
2017-02-18 05:32:02 +0500

    Fixed routes path

--- system/Skeleton/app/Providers/RouteServiceProvider.php
+++ system/Skeleton/app/Providers/RouteServiceProvider.php
@@ -14,7 +14,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    protected $namespace = 'App\Http\Controllers';
+    protected $namespace = 'Restvel\App\Http\Controllers';
 
     /**
      * Define your route model bindings, pattern filters, etc.
@@ -53,7 +53,7 @@ class RouteServiceProvider extends ServiceProvider
     {
         Route::middleware('web')
              ->namespace($this->namespace)
-             ->group(base_path('routes/web.php'));
+             ->group(base_path('system/Head/navigator/web.php'));
     }
 
     /**
@@ -68,6 +68,6 @@ class RouteServiceProvider extends ServiceProvider
         Route::prefix('api')
              ->middleware('api')
              ->namespace($this->namespace)
-             ->group(base_path('routes/api.php'));
+             ->group(base_path('system/Head/navigator/api.php'));
     }
 }
